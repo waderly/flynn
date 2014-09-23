1fa1bda7a6cfdef7c298b136c95e98a92167b944
Jesse Stuart <jesse@jessestuart.ca>
2014-09-23 17:49:55 -0400

    controller/examples: Keys CRUD
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- controller/examples/examples.go
+++ controller/examples/examples.go
@@ -34,6 +34,11 @@ func main() {
 		examples: make(map[string]*request),
 	}
 
+	keyId := e.createKey()
+	e.getKey(keyId)
+	e.listKeys()
+	e.deleteKey(keyId)
+
 	e.createApp()
 	e.getApp()
 	e.updateApp()
@@ -61,6 +66,11 @@ func main() {
 	}
 }
 
+func generatePublicKey() (string, error) {
+	key := `ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPI19fkFmPNg3MGqJorFTbetPJjxlhLDUJFALYe5DyqW0lAnb2R7XvXzj+kRX9LkwOeQjf6nM4bcXbd/H3YPlMDc9JfDuSGlwvo0X8KUQ6PopgyfQ15GA+8YDgwYcBJowIXqAc52GVNnBUeoZzBKvNnsVjAw6KkTPS0aZ6KBZadtYx+Y1fJJBoygh/gtPZ/MQry3XQRvbKPa0iU34Wcx8pXx5QVFLHvyORczQlEVyq5qa5DT86CRR/wC4yH32hkNGalGXY7sZg0j4EY4AeD2yCcmsp7hTt4Ql4gRp3r04ye4DZ7epdXW2tp2vJ3IVn+l6BSNooBIfoD7ZdkUVce51z some-comment`
+	return key, nil
+}
+
 func (e *exampler) DoNewRequest(method, path string, header http.Header, body io.Reader) (*http.Response, error) {
 	url := "http://" + e.conf.controllerDomain + path
 	req, err := e.NewRequest(method, url, header, body)
@@ -84,6 +94,44 @@ func (e *exampler) createResource(path string, body io.Reader) (*http.Response, 
 	header := http.Header{}
 	header.Add("Content-Type", "application/json")
 	return e.DoNewRequest("POST", path, header, body)
+}
+
+func (e *exampler) createKey() string {
+	key, err := generatePublicKey()
+	res, err := e.createResource("/keys", strings.NewReader(fmt.Sprintf(`{
+    "key": "ssh-rsa %s"
+  }`, key)))
+	var k ct.Key
+	dec := json.NewDecoder(res.Body)
+	if err = dec.Decode(&k); err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+	e.examples["key_create"] = getRequests()[0]
+	return k.ID
+}
+
+func (e *exampler) getKey(keyId string) {
+	res, err := e.DoNewRequest("GET", "/keys/"+keyId, nil, nil)
+	if err == nil {
+		io.Copy(ioutil.Discard, res.Body)
+	}
+	e.examples["key_get"] = getRequests()[0]
+}
+
+func (e *exampler) listKeys() {
+	res, err := e.DoNewRequest("GET", "/keys", nil, nil)
+	if err == nil {
+		io.Copy(ioutil.Discard, res.Body)
+	}
+	e.examples["key_list"] = getRequests()[0]
+}
+
+func (e *exampler) deleteKey(keyId string) {
+	res, err := e.DoNewRequest("DELETE", "/keys/"+keyId, nil, nil)
+	if err == nil {
+		io.Copy(ioutil.Discard, res.Body)
+	}
+	e.examples["key_delete"] = getRequests()[0]
 }
 
 func (e *exampler) createApp() {
