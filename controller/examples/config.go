e37ab6ca95e1196397ebc84ff2b35f7463629775
Jesse Stuart <jesse@jessestuart.ca>
2014-09-26 13:57:41 -0400

    controller/examples: Discover addr
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- controller/examples/config.go
+++ controller/examples/config.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
+	"strings"
 )
 
 type config struct {
 	controllerDomain string
 	controllerKey    string
+	ourAddr          string
 }
 
 func loadConfigFromEnv() (c *config, err error) {
@@ -15,10 +18,71 @@ func loadConfigFromEnv() (c *config, err error) {
 	c.controllerDomain = os.Getenv("CONTROLLER_DOMAIN")
 	if c.controllerDomain == "" {
 		err = fmt.Errorf("CONTROLLER_DOMAIN is required")
+		return nil, err
 	}
 	c.controllerKey = os.Getenv("CONTROLLER_KEY")
 	if c.controllerKey == "" {
 		err = fmt.Errorf("CONTROLLER_KEY is required")
+		return nil, err
 	}
-	return c, err
+	c.ourAddr = os.Getenv("ADDR")
+	if c.ourAddr == "" {
+		err = c.discoverAddr()
+		if err != nil {
+			err = fmt.Errorf("Discovery failed, ADDR is required")
+			return nil, err
+		}
+	}
+	return c, nil
+}
+
+func (c *config) discoverAddr() error {
+	controllerAddrs, err := net.LookupHost(c.controllerDomain)
+	if err != nil {
+		return err
+	}
+	ints, err := net.Interfaces()
+	if err != nil {
+		return err
+	}
+	addrs := make([]string, 0, len(ints))
+	for _, i := range ints {
+		iAddrs, err := i.Addrs()
+		if err != nil {
+			continue
+		}
+		for _, addr := range iAddrs {
+			addrs = append(addrs, addr.String())
+		}
+	}
+	var ourAddr string
+	for _, cAddr := range controllerAddrs {
+		cAddrParts := strings.Split(cAddr, ".")
+		if len(cAddrParts) != 4 {
+			continue
+		}
+		for _, iAddr := range addrs {
+			iAddrParts := strings.Split(iAddr, ".")
+			if len(iAddrParts) != 4 {
+				continue
+			}
+			if cAddrParts[0] != iAddrParts[0] {
+				continue
+			}
+			if cAddrParts[1] != iAddrParts[1] {
+				continue
+			}
+			if cAddrParts[2] != iAddrParts[2] {
+				continue
+			}
+			ourAddr = iAddr
+			break
+		}
+	}
+	if ourAddr == "" {
+		return fmt.Errorf("No interface found")
+	}
+	ourAddr = strings.Split(ourAddr, "/")[0]
+	c.ourAddr = ourAddr
+	return nil
 }
