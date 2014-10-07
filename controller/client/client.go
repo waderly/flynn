982cac09557950e742b6cddc5b8a2b7027272470
Jesse Stuart <jesse@jessestuart.ca>
2014-10-07 12:46:37 +0100

    controller/client: Add GetKey method
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- controller/client/client.go
+++ controller/client/client.go
@@ -421,6 +421,11 @@ func (c *Client) CreateKey(pubKey string) (*ct.Key, error) {
 	return key, c.post("/keys", &ct.Key{Key: pubKey}, key)
 }
 
+func (c *Client) GetKey(keyID string) (*ct.Key, error) {
+	key := &ct.Key{}
+	return key, c.get(fmt.Sprintf("/keys/%s", keyID), key)
+}
+
 func (c *Client) DeleteKey(id string) error {
 	return c.delete("/keys/" + strings.Replace(id, ":", "", -1))
 }
